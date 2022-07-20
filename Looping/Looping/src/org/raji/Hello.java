04a16a06ef2cba2a0ac4387a744f4bf38e1af846
Rajeshe21 <e.rajesh.gym@gmail.com>
2022-07-20 15:14:41 +0530

    looping1

---
+++ Looping/Looping/src/org/raji/Hello.java
@@ -0,0 +1,73 @@
+package org.raji;
+
+public class Hello {
+	public static void main(String[] args) {
+	    for (int i = 1; i <= 100; i++) {
+	      if (i == 5) {
+	    	continue;
+	      }
+	      System.out.println(i);
+
+	    }
+
+		 System.out.println("======================================");
+	 for (int i = 1; i <= 3; i++) {
+		 //System.out.println(i);
+	      for (int j = 1; j <= 3; j++) {
+	        System.out.println(j);
+	      }
+
+	    }
+	 System.out.println("======================================");
+	 for (int i = 1; i <= 3; i++) {
+	      for (int j = 1; j <= 3; j++) {
+	        System.out.println(i);
+	      }
+
+	    }	 
+	 System.out.println("======================================");
+	 for (int i = 1; i <= 3; i++) {
+	      for (int j = 1; j <= i; j++) {
+	        System.out.println(j);
+	      }
+
+	    }
+	 System.out.println("======================================");
+	 for (int i = 1; i <= 3; i++) {
+	      for (int j = i + 1; j <= 3; j++) {
+	        System.out.println(j);
+	      }
+
+	    }
+	 System.out.println("======================================");
+	 //int i=5;
+     //if (i == 5) {
+     //  break;
+    // }
+    // System.out.println(i);
+	 for (int i = 1; i <= 3; i++) {
+	      for (int j = i + 1; j <= 3; j++) {
+	        System.out.println(i);
+	      }
+	 
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+
+	  }
+
+}
